0f84fcc853b17a9ab32e6fc9224f271b950ef42d
Ben Loy <bennbenbenl@gmail.com>
2022-08-23 22:30:45 +0800

    functional test 2

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 4000;
 
 connectDB();
 
+app.get("/hello-world/test-api", (req, res) => {
+  res.send("hello world")
+});
+
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });

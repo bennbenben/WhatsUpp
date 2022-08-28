d382145e2f0477636dec4346d72175858f706b3a
bobbest <banditben.1994@gmail.com>
2022-08-29 02:58:14 +0800

    feat[chat]: created sidebar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,36 @@
 import "./App.css";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/users/Login";
 import Register from "./components/users/Register";
 import Chat from "./components/chat/Chat";
+import Sidebar from "./components/chat/Sidebar";
 
-const fetchHelloWorld = () => {
-  fetch("/hello-world/test-api")
-  .then(resp => {
-    console.log(resp);
-  })
-}
-
+// const fetchHelloWorld = () => {
+//   fetch("/hello-world/test-api").then((resp) => {
+//     console.log(resp);
+//   });
+// };
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register" exact element={<Register />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/" exact element={<Chat/>} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <div className="app">
+        <h1>Lets build a whatsapp chat</h1>
+        <div className="app__body">
+          <Sidebar />
+          <Chat />
+        </div>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" exact element={<Register />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/testing" exact element={<Chat />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;

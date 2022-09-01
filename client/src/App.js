1ca16d76f440cfb488c35a16494871c92fffd93f
Ben Loy <bennbenbenl@gmail.com>
2022-09-01 09:54:15 +0800

    feat(Users): Add basic FE pages under components/users/{Login.jsx, Register.jsx}

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
-import "./App.css";
+// React methods
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Routing
+import PrivateRoute from "./components/routing/PrivateRoute"; 
+
+// Components
 import Login from "./components/users/Login";
 import Register from "./components/users/Register";
 import Chat from "./components/chat/Chat";
 import Sidebar from "./components/chat/Sidebar";
+import HelloWorld from "./components/users/HelloWorld";
+import Hello from "./components/users/Hello";
 
-// const fetchHelloWorld = () => {
-//   fetch("/hello-world/test-api").then((resp) => {
-//     console.log(resp);
-//   });
-// };
+// Styles
+import "./App.css";
+import Private from "./components/users/Private";
 
 const App = () => {
+  console.log("APP.JS RENDER")
   return (
     <div>
       <div className="app">
@@ -23,9 +29,16 @@ const App = () => {
 
         <BrowserRouter>
           <Routes>
-            <Route path="/register" exact element={<Register />} />
-            <Route path="/login" exact element={<Login />} />
-            <Route path="/testing" exact element={<Chat />} />
+            <Route element={<PrivateRoute />} >
+              <Route index element={<Hello />} />
+              <Route path="/helloworld" element={<HelloWorld />} />
+              <Route path="/private" element={<Private />} />
+            </Route>
+
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* <Route exact path="/forgotpassword" element={<ForgotPassword />} /> */}
+            {/* <Route exact path="/passwordreset/:resetToken" element={<ResetPassword />} /> */}
           </Routes>
         </BrowserRouter>
       </div>
--- client/src/components/users/Login.jsx
+++ client/src/components/users/Login.jsx
@@ -1,26 +1,78 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Login.css";
 
 const Login = () => {
-  const handleSubmit = (e) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  // const [token, setToken] = useState(localStorage.getItem("authToken"));
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      navigate("/");
+    }
+  }, []);
+
+    // if (localStorage.getItem("authToken")) {
+    //   console.log("this is working");
+    //   navigate("/");
+    // }
+
+  const loginHandler = async (e) => {
     e.preventDefault();
-    console.log(e);
+
+    const axiosConfig = {
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const response = await axios.post(
+        "/api/v1/login", { email, password }, axiosConfig
+      );
+      console.log(`response.data: ${response.data}`);
+      localStorage.setItem("authToken", response.data.token);
+      
+      // setToken(response.data.token);
+      navigate("/");
+      
+    } catch (error) {
+      // console.log(`error: ${error}`);
+      setError(error.response.data.error);
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
   };
 
   return (
-    <div style={{ width: '100%', height: '90vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <form onSubmit={handleSubmit} style={{background: 'grey', padding: '32px'}}>
-       <div>
-       <h3>Username</h3>
-        <input type="text" />
-       </div>
-        <div>
-        <h3>Password</h3>
-        <input type="password" />
+    <div className="login-view">
+      <form className="login-view__form" onSubmit={loginHandler}>
+        <h3 className="login-view__title">Login</h3>
+
+        {error && <span className="error-message">{error}</span>}
+
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" required id="email" placeholder="Enter email here" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
-        <button style={{marginTop: '32px'}} type="submit">Login</button>
+
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" required id="password" placeholder="Enter password here" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Login</button>
+
+        <span className="login-view__subtext">
+          Do not have an account?
+          <Link to="/register">Register</Link>
+        </span>
       </form>
-      </div>
     </div>
   );
 };

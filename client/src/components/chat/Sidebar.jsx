cf73c0ca70857b738021bfc26c74748b3ed6fd67
Ben Loy <bennbenbenl@gmail.com>
2022-09-07 00:19:22 +0800

    feat(Chat): add Sidebar.jsx and Chat.jsx routing into App.js

--- client/src/components/chat/Sidebar.jsx
+++ client/src/components/chat/Sidebar.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import SidebarChat from "./SidebarChat";
+// Import libraries
 import { useEffect, useState } from "react";
-
-import DonutLargeIcon from "@mui/icons-material/DonutLarge";
-import ChatIcon from "@mui/icons-material/Chat";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Avatar, IconButton } from "@mui/material";
+import ChatIcon from "@mui/icons-material/Chat";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
+// Import internal components
 import "./Sidebar.css";
+import SidebarChat from "./SidebarChat";
 import { useStateValue } from "../../StateProvider";
 
-function Sidebar() {
+const Sidebar = () => {
   const [rooms, setRooms] = useState([]);
   const [{ user }, dispatch] = useStateValue()
 
@@ -38,33 +38,29 @@ function Sidebar() {
   ));
 
   return (
-    <div className="sidebar">
+    <div className="sidebar__main">
       <div className="sidebar__header">
         <Avatar src={user?.avatar}/>
         <div className="sidebar__headerRight">
-          <IconButton>
-            <DonutLargeIcon />
-          </IconButton>
-          <IconButton>
-            <ChatIcon />
-          </IconButton>
-          <IconButton>
-            <MoreVertIcon />
-          </IconButton>
+          <IconButton><DonutLargeIcon /></IconButton>
+          <IconButton><ChatIcon /></IconButton>
+          <IconButton><MoreVertIcon /></IconButton>
         </div>
       </div>
+      
       <div className="sidebar__search">
         <div className="sidebar__searchContainer">
           <SearchOutlinedIcon />
           <input placeholder="Search or start new chat" type="text" />
         </div>
       </div>
+      
       <div className="sidebar__chats">
         <SidebarChat addNewChat />
         {displayRooms}
       </div>
     </div>
   );
-}
+};
 
 export default Sidebar;

63940ad3640b01502d6c85df7703d87baef13a39
Ben Loy <bennbenbenl@gmail.com>
2022-09-20 01:27:45 +0800

    bugfix(root page): fixed a bug where root page is not navigating to /login upon first page-load [FE]

--- client/src/components/routing/PrivateRoute.jsx
+++ client/src/components/routing/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 // Import libraries
 import { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 
 // Import internal components
 import { Store } from "../../data/Store";
@@ -9,25 +9,32 @@ import { useEffect } from "react";
 
 const PrivateRoute = () => {
   const [globalState, dispatch] = useContext(Store);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const authToken = localStorage.getItem("authToken");
-    const base64Payload = JSON.stringify(authToken).split(".")[1];
-    const currentUser = JSON.parse(window.atob(base64Payload));
-    console.log("currentUser in privateroute: ", currentUser);
+    if (!localStorage.getItem("authToken")) {
+      navigate("/login");
 
-    const expiryDate = new Date(currentUser.exp * 1000);
-    const currentDate = new Date(Date.now());
+    } else {
+      const authToken = localStorage.getItem("authToken");
+      const base64Payload = JSON.stringify(authToken).split(".")[1];
+      console.log("base64Payload:", base64Payload)
+      const currentUser = JSON.parse(window.atob(base64Payload));
+      console.log("currentUser in privateroute: ", currentUser);
 
-    if ( expiryDate <= currentDate ) {
-      localStorage.removeItem("authToken");
-      dispatch(userLogout());
-      alert("token expired. please login again");
-    }
-    
-    dispatch(userLoginSuccess(currentUser));
-    console.log('dispatch from Private route happened')
-  },[]);
+      const expiryDate = new Date(currentUser.exp * 1000);
+      const currentDate = new Date(Date.now());
+
+      if ( expiryDate <= currentDate ) {
+        localStorage.removeItem("authToken");
+        dispatch(userLogout());
+        alert("token expired. please login again");
+      }
+      
+      dispatch(userLoginSuccess(currentUser));
+      console.log('dispatch from Private route happened');
+  }
+  }, []);
 
   // If first time login, - JWT dont exist, context dont exist, navigate them to /login
   if (!localStorage.getItem("authToken")) {
